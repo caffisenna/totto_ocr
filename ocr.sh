f21465a6ab8f3e9ae4d6d71136fd2bef1714ab8c
caffi <caffi.senna@gmail.com>
2021-08-22 13:56:30 +0900

    Update ocr.sh
    ・黒文字以外を白で埋める
    ・スキャン方向を水平に

--- ocr.sh
+++ ocr.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
 # path to image file
-path="bw2.jpg"
+path="activityList.png"
+
+# 加工
+# convert -monochrome a.png bw.png
+# convert bw.png -negate wb.png
+convert -fill '#ffffff' +opaque '#333333' activityList.png activityListWhite.png
 
 # tmpfile
-tmp_file=$(mktemp)
+# tmp_file=$(mktemp)
+tmp_file=tmp.txt
 
 # result
 result_file="result.txt"
 
 # command
-command="/usr/bin/tesseract $path stdout -l eng"
+command="/usr/bin/tesseract activityListWhite.png stdout -l eng --psm 6"
 
 # コマンド実行して一時ファイル生成
 $command > $tmp_file
@@ -23,4 +29,4 @@ sed -n 23p $tmp_file >> $result_file
 sed -e 's/[A-Z]//ig' -e 's/ //g' -i $result_file
 
 # 掃除
-rm $tmp_file
+# rm $tmp_file
